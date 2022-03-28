4216d600c76d62489f9500b85adcd4e3d76c6ff3
RedMellon <gamevancony@gmail.com>
2022-03-27 22:58:54 -0700

    added colorama and start text

--- wordle.py
+++ wordle.py
@@ -1,1 +1,28 @@
-# 
+from time import sleep
+from colorama import init
+from colorama import Fore, Back, Style
+# print(Fore.RED + 'some red text')
+# print(Back.GREEN + 'and with a green background')
+# print(Style.RESET_ALL)
+# print('back to normal now')
+init() # initializes colorama
+
+def coloramaDemo():
+    print(Fore.GREEN + "C" + "R" +
+            Fore.YELLOW + "A" + "N" +
+            Fore.RESET + "E")
+
+coloramaDemo()
+
+print("Welcome to the Wordle Simulator!")
+print()
+print("Wordle is a game where you have 6 tries to guess a 5-letter word.")
+print("After every guess, you are given some clues to show how close you are to the word:")
+print(Fore.GREEN + "GREEN LETTER" + Fore.RESET + " - letter is in word and in correct spot")
+print(Fore.YELLOW + "YELLOW LETTER" + Fore.RESET + " - letter is in word but in wrong spot")
+print("NORMAL LETTER - letter is not in word")
+print()
+print("This program is based off of the original Wordle game, by Josh Wardle. You can find it here!")
+print("https://www.nytimes.com/games/wordle/index.html")
+print()
+input("Start game? (y/n): ")

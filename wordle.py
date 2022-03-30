110a9680ec3d6e1139aa2d9f8ca0e23c17816a09
RedMellon <gamevancony@gmail.com>
2022-03-29 23:00:48 -0700

    Created main program

--- wordle.py
+++ wordle.py
@@ -1,35 +1,51 @@
-from time import sleep
-from colorama import init
-from colorama import Fore, Back, Style
-# print(Fore.RED + 'some red text')
-# print(Back.GREEN + 'and with a green background')
-# print(Style.RESET_ALL)
-# print('back to normal now')
-init() # initializes colorama
+# import python modules
+#from time import sleep
+from colorama import init, Fore
+init()
 
-def coloramaDemo():
-    print(Fore.GREEN + "C" + "R" +
-            Fore.YELLOW + "A" + "N" +
-            Fore.RESET + "E")
+# UTLITY FUNCTIONS
+def print_intro():
+    print()
+    print("Welcome to the Wordle Simulator!")
+    print()
+    print("Wordle is a game where you have 6 tries to guess a 5-letter word.")
+    print("After every guess, you are given some clues to show how close you are to the word:")
+    print(Fore.GREEN + "GREEN LETTER" + Fore.RESET + " - letter is in word and in correct spot")
+    print(Fore.YELLOW + "YELLOW LETTER" + Fore.RESET + " - letter is in word but in wrong spot")
+    print("NORMAL LETTER - letter is not in word")
+    print()
+    print("This program is based off of the original Wordle game by Josh Wardle. You can find it here!")
+    print("https://www.nytimes.com/games/wordle/index.html")
+    print()
 
-coloramaDemo()
 
-print("Welcome to the Wordle Simulator!")
-print()
-print("Wordle is a game where you have 6 tries to guess a 5-letter word.")
-print("After every guess, you are given some clues to show how close you are to the word:")
-print(Fore.GREEN + "GREEN LETTER" + Fore.RESET + " - letter is in word and in correct spot")
-print(Fore.YELLOW + "YELLOW LETTER" + Fore.RESET + " - letter is in word but in wrong spot")
-print("NORMAL LETTER - letter is not in word")
-print()
-print("This program is based off of the original Wordle game, by Josh Wardle. You can find it here!")
-print("https://www.nytimes.com/games/wordle/index.html")
-print()
-input("Start game? (y/n): ")
-
-# may be used
+# MAIN PROGRAM
 def main():
-    print("whale")
+    # print intro text
+    print_intro()
+    # initial prompt
+    play = input("Start game? (y/n): ")
+    if play.lower() == "y":
+        # starts main game loop
+        while True:
+            # pick mystery word in word bank
 
+            # prompt to play again
+            play_again = input("Play again? (y/n): ")
+            if play_again.lower() == "y":
+                continue
+            elif play_again.lower() == "n":
+                print("Thanks for playing! Exiting program.")
+                break
+            else:
+                print("Invalid input. Exiting program.")
+                break
+
+    elif play.lower() == "n":
+        print("Exiting. Bye!")
+    else:
+        print("Invalid response. Exiting.")
+
+# PROGRAM RUNNER
 if __name__ == "__main__":
     main()
